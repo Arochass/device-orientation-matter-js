8ae66c06fc7067947066bac31fa41ce67d2c202e
arochass <you@example.com>
2022-08-31 12:33:53 +0700

    wall

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ window.onload = () => {
   var game = {
     angle: 0, //keeps track of rotations
     width: window.innerWidth, //400, //window.innerWidth-20,//1200,
-    height: window.innerHeight, //600, //window.innerHeight-20, //800,
+    height: 2*window.innerHeight, //600, //window.innerHeight-20, //800,
   }
 
   // module aliases
