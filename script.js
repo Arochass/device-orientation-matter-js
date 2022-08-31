8ccc1be75f9e02ae22d2134aedc086406ff92501
arochass <you@example.com>
2022-08-31 12:51:36 +0700

    wall

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ window.onload = () => {
     ), //top
     Bodies.rectangle(
       game.width * 0.5,
-      game.height + wallSettings.size * 0.5,
+      game.height*0.5 + wallSettings.size * 0.5,
       game.width,
       wallSettings.size,
       wallSettings
